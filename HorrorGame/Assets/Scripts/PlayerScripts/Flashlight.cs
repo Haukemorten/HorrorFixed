accb7eddf6a8f384175eff4ac984b25215d4dfb7
Haukemorten <107413393+Haukemorten@users.noreply.github.com>
2022-08-01 15:10:54 +0200

    ++

--- HorrorGame/Assets/Scripts/PlayerScripts/Flashlight.cs
+++ HorrorGame/Assets/Scripts/PlayerScripts/Flashlight.cs
@@ -6,16 +6,38 @@ public class Flashlight : MonoBehaviour
 {
     public GameObject flashlight;
     public bool On;
+    [SerializeField]  float DrainRate;
+    [SerializeField] float currentEnergy;
+    [SerializeField] float maxEnergy;
+     public bool Drained;
 
     // Start is called before the first frame update
     void Start()
     {
-        flashlight.SetActive(false);
+
+        flashlight.SetActive(false);    
     }
 
     // Update is called once per frame
     void Update()
     {
+         
+        if (currentEnergy <= 0)
+        {
+            if (On) 
+            {
+                StartCoroutine(TurnOff()); 
+            }
+            return;   
+        
+            
+        
+        }
+        if (On) 
+        {
+            currentEnergy -= Time.deltaTime * DrainRate;
+        
+        }
 
         if (Input.GetKeyDown("f") & On == true)
         {
@@ -32,6 +54,7 @@ public class Flashlight : MonoBehaviour
 
     IEnumerator TurnOff()
     {
+
         yield return new WaitForSeconds(0.1f);
         flashlight.SetActive(false);
         On = false;
