e4e651a5aaf83dd7dfdc9622d2d1a2484ef5f9e0
Haukemorten <107413393+Haukemorten@users.noreply.github.com>
2022-08-09 17:00:52 +0200

    tasrget Improve

--- HorrorGame/Assets/Scripts/Events/EnemyMovement.cs
+++ HorrorGame/Assets/Scripts/Events/EnemyMovement.cs
@@ -11,8 +11,16 @@ public class EnemyMovement : MonoBehaviour
     private float TargetDistance;
     private int TargetNumber = 1;
 
+    [SerializeField] int MaxTargets =8;
     [SerializeField] Transform Target1;
     [SerializeField] Transform Target2;
+    [SerializeField] Transform Target3;
+    [SerializeField] Transform Target4;
+    [SerializeField] Transform Target5;
+    [SerializeField] Transform Target6;
+    [SerializeField] Transform Target7;
+    [SerializeField] Transform Target8;
+
     [SerializeField] float StopDistance = 2.0f;
     // Start is called before the first frame update
     void Start()
@@ -33,7 +41,7 @@ public class EnemyMovement : MonoBehaviour
         if (TargetDistance < StopDistance)
         {
             TargetNumber++;
-            if (TargetNumber > 2) 
+            if (TargetNumber > MaxTargets) 
             {
                 TargetNumber =1;
             }
@@ -51,6 +59,30 @@ public class EnemyMovement : MonoBehaviour
         {
             Target = Target2;
         }
-    
+        if (TargetNumber == 3)
+        {
+            Target = Target3;
+        }
+        if (TargetNumber == 4)
+        {
+            Target = Target4;
+        }
+        if (TargetNumber == 5)
+        {
+            Target = Target5;
+        }
+        if (TargetNumber == 6)
+        {
+            Target = Target6;
+        }
+        if (TargetNumber == 7)
+        {
+            Target = Target7;
+        }
+        if (TargetNumber == 8)
+        {
+            Target = Target8;
+        }
+
     }
 }
